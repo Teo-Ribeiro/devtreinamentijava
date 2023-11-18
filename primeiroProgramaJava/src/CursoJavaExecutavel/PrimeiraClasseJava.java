64e7fa871a19f294421832fda7abc99f9c82bfaf
Teo-Ribeiro <teodoro.l.ribeiro@gmail.com>
2023-11-18 20:30:18 -0300

    criacao do projeto aula java e github

---
+++ primeiroProgramaJava/src/CursoJavaExecutavel/PrimeiraClasseJava.java
@@ -0,0 +1,18 @@
+package CursoJavaExecutavel;
+
+public class PrimeiraClasseJava {
+	/*main é um metodo autoexecutavel em java*/
+	public static void main(String[] args) {
+		
+		System.out.println("Meu primeiro programa em Java");
+		System.out.println("jdev treinamentos");
+		
+		
+		
+	}
+	
+	
+	
+	
+
+}
